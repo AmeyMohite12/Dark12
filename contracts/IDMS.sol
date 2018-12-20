82c1b5e839c7869a2c2c46c5604234392f038717
Ameya <ameymohite99@gmail.com>
2018-12-20 22:51:51 +0530

    Recruiter and Student on same Sol

--- contracts/IDMS.sol
+++ contracts/IDMS.sol
@@ -8,17 +8,13 @@ contract IDMS {
 	
 	
 	struct StudentProfile{
+		address StudentAddress;
 		string StudentName;
 		uint Age;
 		uint256 birth_date;
 		uint CGPA;
 		string Institute;
 		uint256 Contact;		
-
-		 
-
-
-
 	}
 
 	mapping(uint => StudentProfile)public StudentId;
@@ -36,19 +32,34 @@ contract IDMS {
 		StudentCount++;
 		//StudentId[StudentCount] = StudentProfile(_name,_age);
 	}
-
-
-	
-
-
-
-
-
-
-
 	// getter functions
-	function getName() public view returns(string){
-		return StudentId[1].StudentName;
+	//function getName() public view returns(string){
+	//	return (StudentId[1].StudentName,.....);
+
+	//}
+}
+
+contract Recruiter {
+
+	struct Recruit {
+		//address RecruiterAddress;
+		string OrganisationName;
+		uint256 Contact;
+			
 
 	}
+	
+	mapping(uint=>Recruit) public RecruiterId;
+	uint public RecruiterCount;
+
+	function Recruiter () {
+		addRecruiter("Hekko",9192929299);
+		
+	}
+
+
+	function addRecruiter(string _name,uint256 _contact) public{
+		RecruiterCount++;
+		RecruiterId[RecruiterCount] = Recruit(_name,_contact);
+	}	
 }

d26816f02b4e774d4376b47f32795d8e0803a2ab
Ameya <ameymohite99@gmail.com>
2018-12-20 19:16:02 +0530

    IDMS DEVELOPMENT BEGINS NOW

--- contracts/IDMS.sol
+++ contracts/IDMS.sol
@@ -5,8 +5,33 @@ pragma solidity ^0.4.19;
 
  
 contract IDMS {
-	uint name;
-	constructor() public {	
-			name = 50;
-	}	
+	
+	
+	struct StudentProfile{
+		string StudentName;
+		uint Age;
+		//uint CGPA;
+
+	}
+
+	mapping(uint => StudentProfile)public StudentId;
+	uint public StudentCount;
+
+
+	constructor () public {	
+		addStudent("Amy",21);
+		addStudent("Adams",22);
+			
+	}
+
+	function addStudent(string _name, uint _age) public
+	{
+		StudentCount++;
+		StudentId[StudentCount] = StudentProfile(_name,_age);
+	}
+
+	function getName() public view returns(string){
+		return StudentId[0].StudentName;
+
+	}
 }
--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -3,3 +3,6 @@ var IDMS = artifacts.require("./IDMS.sol");
 module.exports = function(deployer) {
   deployer.deploy(IDMS);
 };
+
+
+// IDMS.deployed().then(function(instance) { app = instance })
